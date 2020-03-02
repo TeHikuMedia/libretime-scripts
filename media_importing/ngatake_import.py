c3ab6c6b7a229274956a45325ecdc7b9b63391ed
Keoni Mahelona <keoni@kmahelona.net>
2020-03-02 18:32:10 +1300

    Update for python 3

--- media_importing/ngatake_import.py
+++ media_importing/ngatake_import.py
@@ -1,5 +1,5 @@
 import taglib
-import urllib2
+import urllib
 from datetime import datetime, timedelta
 import time
 
@@ -28,7 +28,7 @@ start_time = datetime.now()
 url = 'https://tehiku.nz/api/te-reo/nga-take/latest?mp3=True'
 url2 = 'https://tehiku.nz/api/te-reo/nga-take/latest'
 target_length = 60.0*3.0 + 45.0
-response = urllib2.urlopen(url2)
+response = urllib.request.urlopen(url2)
 # headers = response.info().headers
 # for item in headers:
 #     if 'content-disposition' in item.lower():
@@ -37,14 +37,14 @@ data = response.read()
 j = json.loads(data)
 
 utc = datetime.strptime(j['publish_date'], '%Y-%m-%dT%H:%M:%SZ')
-print utc
+print(utc)
 dt = utc2local(utc)
-print dt
+print(dt)
 
 title = dt.strftime('%d%m%y')
 PM = dt.strftime('%p')
 
-print title
+print(title)
 
 f_name = 'Nga_Take_{0}.mp3'.format(PM)
 f_path = path.join(BASE_MEDIA_DIR, 'tehiku_ngatake')
@@ -60,7 +60,7 @@ p.communicate()
 get_new_file = True
 
 if get_new_file:
-    print "Downloading new file..."
+    print("Downloading new file...")
     # check if we need to update the file
 
     media_length = scale_media(f_name, target_length)
@@ -79,27 +79,27 @@ if get_new_file:
     fd.tags['LENGTH'] = "%d:%02d.%d"%(media_length['mins'], media_length['secs'], media_length['hunds'])
     fd.tags['TLEN'] = "%d:%02d.%d"%(media_length['mins'], media_length['secs'], media_length['hunds'])
     retval = fd.save()
-    print retval
+    print(retval)
 
     commands.getstatusoutput('sudo chown www-data %s'%(f_name))
     commands.getstatusoutput('sudo chgrp www-data %s'%(f_name))
     commands.getstatusoutput('mv %s %s/'%(f_name,f_path))
 
     td =  (datetime.now() - start_time)
-    print 'elapsed time = %s' % ( td.seconds )
+    print('elapsed time = %s' % ( td.seconds ))
 
     # Try to force move file into airtime shit
     # try:
     #     dest_file = commands.getstatusoutput('sudo cat /var/log/airtime/pypo/pypo.log | grep copy.*%s.*/scheduler/'%(f_name))[1].split('\n')[0].split('/scheduler/')[1].split('.mp3')[0]+'.mp3'
-    #     print "Copying %s to .../scheduler/%s" %(f_name, dest_file)
+    #     print("Copying %s to .../scheduler/%s" %(f_name, dest_file))
 
     #     success = commands.getstatusoutput('sudo cp -v %s/%s /var/tmp/airtime/pypo/cache/scheduler/%s'%(f_path,f_name,dest_file))
     #     commands.getstatusoutput('sudo chown www-data /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
     #     commands.getstatusoutput('sudo chgrp www-data /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
     #     commands.getstatusoutput('sudo chmod a+rw /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
-    #     for i in success: print i
+    #     for i in success: print(i)
     # except:
-    #     print "Something went wrong while trying to fudge Airtime"
+    #     print("Something went wrong while trying to fudge Airtime")
 
 else:
     commands.getstatusoutput('rm %s'%(f_name) )
--- media_importing/waatea_import.py
+++ media_importing/waatea_import.py
@@ -1,5 +1,5 @@
 import taglib
-import urllib2
+
 from datetime import datetime, timedelta
 import commands 
 from math import floor
@@ -35,7 +35,7 @@ for item in items:
     if 'news sport %s%s'%(hour,ampm) in item:
         f_id = item.split(' ')[0].strip()
 if f_id == '':
-    print "No News for this Hour"
+    print("No News for this Hour")
     raise NameError('news sport %s%s'%(hour,ampm))
 
 f_name = 'Waatea_News_%s%s.mp3'%(hour,ampm)
@@ -48,7 +48,7 @@ if not os.path.exists(tmp_path):
     os.mkdir(tmp_path)
 
 target_length = 60*6.0
-print "Fetching %s"%(f_name)
+print("Fetching %s"%(f_name))
 
 # Remove the file?
 # commands.getstatusoutput('rm %s/%s'%(f_path, f_name))
@@ -62,7 +62,7 @@ for line in xml:
         r_date = line.replace('<recorded>','').replace('</recorded>','').strip()
 
 record_date = datetime.strptime(r_date,'%m/%d/%Y %H:%M:%S')
-print "Recorded", record_date
+print("Recorded", record_date)
 
 get_new_file = False
 # get current file '/srv/airtime/watch_folder/waatea_news/%s' % (f_name)
@@ -75,24 +75,24 @@ if path.isfile('%s/%s'%(f_path, f_name)):
     except ValueError:
         file_record_date = datetime.strptime(fd.tags[u'DATE'][0], '%Y-%m-%d')
         
-    print "Old recorded", file_record_date
+    print("Old recorded", file_record_date)
     if int(record_date.strftime('%Y%m%d%H%M%S')) > int(file_record_date.strftime('%Y%m%d%H%M%S')):
-        print "File needs updating..."
+        print("File needs updating...")
         get_new_file = True
     else:
         if hour==7 and ampm=='a':
             if int(record_date.strftime('%Y%m%d')) < int(time.strftime('%Y%m%d')):
-                print "File needs updating..."
+                print("File needs updating...")
             get_new_file = True
         else:
-            print "File up to date."
+            print("File up to date.")
             get_new_file = False
 else:
-    print "File doesn't exist..."
+    print("File doesn't exist...")
     get_new_file = True
 
 if get_new_file:
-    print "Downloading new file..."
+    print("Downloading new file...")
     # check if we need to update the file
 
     media_length = scale_media(f_name, target_length)
@@ -111,28 +111,28 @@ if get_new_file:
     fd.tags[u'LENGTH'] = u"%d:%02d.%d"%(media_length['mins'], media_length['secs'], media_length['hunds'])
     fd.tags[u'TLEN'] = u"%d:%02d.%d"%(media_length['mins'], media_length['secs'], media_length['hunds'])
     retval = fd.save()
-    print retval
-    print fd.tags
+    print(retval)
+    print(fd.tags)
 
     commands.getstatusoutput('sudo chown www-data %s'%(f_name))
     commands.getstatusoutput('sudo chgrp www-data %s'%(f_name))
     os.rename(f_name, os.path.join(f_path, f_name))
 
     td =  (datetime.now() - start_time)
-    print 'elapsed time = %s' % ( td.seconds )
+    print('elapsed time = %s' % ( td.seconds ))
 
     # Try to force move file into airtime shit
     # try:
     #     dest_file = commands.getstatusoutput('sudo cat /var/log/airtime/pypo/pypo.log | grep copy.*%s.*/scheduler/'%(f_name))[1].split('\n')[0].split('/scheduler/')[1].split('.mp3')[0]+'.mp3'
-    #     print "Copying %s to .../scheduler/%s" %(f_name, dest_file)
+    #     print("Copying %s to .../scheduler/%s" %(f_name, dest_file))
 
     #     success = commands.getstatusoutput('sudo cp -v %s/%s /var/tmp/airtime/pypo/cache/scheduler/%s'%(f_path,f_name,dest_file))
     #     commands.getstatusoutput('sudo chown www-data /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
     #     commands.getstatusoutput('sudo chgrp www-data /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
     #     commands.getstatusoutput('sudo chmod a+rw /var/tmp/airtime/pypo/cache/scheduler/%s'%(dest_file))
-    #     for i in success: print i
+    #     for i in success: print(i)
     # except:
-    #     print "Something went wrong while trying to fudge Airtime"
+    #     print("Something went wrong while trying to fudge Airtime")
 
 else:
     commands.getstatusoutput('rm %s'%(f_name) )
