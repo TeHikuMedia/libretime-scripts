791e7d3b55dc2dd880c39bd036c3de924a235a5a
Keoni Mahelona <keoni@kmahelona.net>
2020-03-05 19:14:47 +1300

    setup log dir and file

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 import os
+from subprocess import call
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 print(script_path)
 os.chdir(script_path)
 
 data_files = [
-    ('/etc/librescripts', ['install/conf/conf.json']),
-    ('/etc/cron.d',       ['install/cron/update_metadata']),
+    ('/etc/librescripts',     ['install/conf/conf.json']),
+    ('/etc/cron.d',           ['install/cron/update_metadata']),
 ]
 
+LOG_DIR  = '/var/log/librescripts'
+LOG_FILE = os.path.join(LOG_DIR, LOG_FILE)
+# Make sure log directory exists
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+if not os.path.exists(LOG_FILE):
+    open(fname, 'a').close()
 
 setup(
     name="LibreTimeScripts",
