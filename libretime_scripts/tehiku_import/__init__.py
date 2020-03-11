c0e22bd6ba440dbcd2bcdad27fc38a605d4c47b0
Keoni Mahelona <keoni@kmahelona.net>
2020-03-11 20:23:43 +1300

    remove stuff

--- libretime_scripts/tehiku_import/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .import_functions import time_string, convert_media, scale_media
-# from .settings import BASE_MEDIA_DIR
-
-# __all__ = ['time_string', 'convert_media', 'scale_media', 'BASE_MEDIA_DIR']
