a535a20e2a1055cf3bd25413c34e481b830f7b43
Keoni Mahelona <keoni@kmahelona.net>
2020-03-11 20:14:38 +1300

    try init

--- libretime_scripts/tehiku_import/__init__.py
+++ libretime_scripts/tehiku_import/__init__.py
@@ -1,3 +1,3 @@
-# from .import_functions import time_string, convert_media, scale_media
+from .import_functions import time_string, convert_media, scale_media
 
-# __all__ = ['time_string', 'convert_media', 'scale_media']
+__all__ = ['time_string', 'convert_media', 'scale_media']
