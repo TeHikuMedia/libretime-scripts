c925f1acbd9dbc943e95854e8543266ab9ef1cb6
Keoni Mahelona <keoni@kmahelona.net>
2020-03-13 11:42:50 +1300

    add platform checking for settings

--- tehiku_import/settings.py
+++ tehiku_import/settings.py
@@ -1,4 +1,8 @@
-BASE_MEDIA_DIR = "/Users/livestream/Downloads/"
-MD5_CMD = "md5"
-# BASE_MEDIA_DIR = "/home/admin/tehiku_import"
-# MD5_CMD = "md5sum"
+from sys import platform
+
+if platform == 'darwin':
+    BASE_MEDIA_DIR = "/Users/livestream/Downloads/"
+    MD5_CMD = "md5"
+else:
+    BASE_MEDIA_DIR = "/home/admin/tehiku_import"
+    MD5_CMD = "md5sum"
