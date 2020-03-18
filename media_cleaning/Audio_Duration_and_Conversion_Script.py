f514efe02e42d8ef0f6c611a448a8a0a6e677d8a
Keoni Mahelona <keoni@kmahelona.net>
2020-03-18 16:26:41 +1300

    use ok walk

--- media_cleaning/Audio_Duration_and_Conversion_Script.py
+++ media_cleaning/Audio_Duration_and_Conversion_Script.py
@@ -7,7 +7,9 @@ import json
 
 BASE_SOURCE_DIRECTORY = 'Y:\\'
 
-SEARCH_DIR = os.path.join(BASE_SOURCE_DIRECTORY, '\\#WORKING\\Ads\\English\\#Remake')
+# SEARCH_DIR = os.path.join(BASE_SOURCE_DIRECTORY, '\\#WORKING\\Ads\\English\\#Remake')
+SEARCH_DIR = os.path.join(BASE_SOURCE_DIRECTORY, '\\Music')
+DESTINATION_DIR = os.path.join(BASE_SOURCE_DIRECTORY, '\\#WORKING')
 os.chdir(SEARCH_DIR)
 
 audio_files = glob.glob(r'*.mp3')
@@ -19,79 +21,83 @@ audio_files.extend(glob.glob(r'*.flac'))
 
 # count = 0
 
-for file in audio_files:
-    print(file)
-    
-    source = r'Y:\#WORKING\Ads\English\#Remake'
-    output = r'Y:\#WORKING\Ads_checked\English\#Remake'
-    source = os.path.join(source,file)
-    output = os.path.join(output,file)
+for root, dirs, files in os.walk(SEARCH_DIR):
 
-    print(source)
-    print(output)
-    
-    cmd = [
-         'ffprobe',
-         '-i', file,
-         '-show_entries',
-         'format=duration',
-         '-v', 'quiet',
-         '-of', 'json']
-     
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    data, error = p.communicate()
+    for file in files:
 
-    print('Data:\t', data)
-    print('Error: \t', error)
-
-    data = json.loads(data)
-
-
-    duration = float(data['format']['duration'])*1000
-    print('Duration:', duration,'ms')
-
-    try:
-        audio = EasyID3(file)
-        tag_length  = float(audio['length'][0])
-    except IndexError:
-            print('ERROR:     Something is janky...')
-            continue
-    except KeyError:
-        tag_length = None
-    except Exception as error:
-        print(error)
-        continue
+        print(file)
         
-    print('Tag Length', tag_length)
+        source = os.path.join(root, file)
+        BASE_OUT = root.replace(SEARCH_DIR, DESTINATION_DIR)
+        output = os.path.join(root, file)
 
-    if tag_length is None:
-        print("Message:      Tag is None, setting to", duration)
-        run = [
-            'ffmpeg', '-y', 
-            '-i', source,
-            '-codec:a',
-            'copy', 
-            #'-b:a',
-            #'160k', 
-            output]
+        print(source)
+        print(output)
+        
+        break
+        
+        cmd = [
+             'ffprobe',
+             '-i', file,
+             '-show_entries',
+             'format=duration',
+             '-v', 'quiet',
+             '-of', 'json']
+         
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        data, error = p.communicate()
 
-        p = Popen(run)
+        print('Data:\t', data)
+        print('Error: \t', error)
 
-    elif round(duration) != round(tag_length):
-        print("Message:      Tag is different",duration)
-        run = [
-            'ffmpeg', '-y', 
-            '-i', source,
-            '-codec:a',
-            'copy', 
-            #'-b:a',
-            #'160k', 
-            output]
+        data = json.loads(data)
 
-        #p = Popen(run)
 
-    else:
-        print("Message: Tag length is correct.")
+        duration = float(data['format']['duration'])*1000
+        print('Duration:', duration,'ms')
+
+        try:
+            audio = EasyID3(file)
+            tag_length  = float(audio['length'][0])
+        except IndexError:
+                print('ERROR:     Something is janky...')
+                continue
+        except KeyError:
+            tag_length = None
+        except Exception as error:
+            print(error)
+            continue
+            
+        print('Tag Length', tag_length)
+
+        if tag_length is None:
+            print("Message:      Tag is None, setting to", duration)
+            run = [
+                'ffmpeg', '-y', 
+                '-i', source,
+                '-codec:a',
+                'copy', 
+                #'-b:a',
+                #'160k', 
+                output]
+
+            p = Popen(run)
+
+        elif round(duration) != round(tag_length):
+            print("Message:      Tag is different",duration)
+            run = [
+                'ffmpeg', '-y', 
+                '-i', source,
+                '-codec:a',
+                'copy', 
+                #'-b:a',
+                #'160k', 
+                output]
+
+            #p = Popen(run)
+
+        else:
+            print("Message: Tag length is correct.")
 
 
 # *********************************************************
