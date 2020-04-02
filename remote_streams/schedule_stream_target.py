6c938416300be85c8655dc35831ed8e355fe86a3
Keoni Mahelona <keoni@kmahelona.net>
2020-04-02 21:11:15 +1300

    localise timezoen

--- remote_streams/schedule_stream_target.py
+++ remote_streams/schedule_stream_target.py
@@ -32,8 +32,8 @@ HEADERS ={
     'Content-Type': 'application/json; charset=utf-8'
 }
 
-START_TIME = datetime.strptime('2020/04/2 13:00:00', '%Y/%m/%d %H:%M:%S').astimezone(timezone)
-END_TIME = datetime.strptime('2020/04/2 17:00:00', '%Y/%m/%d %H:%M:%S').astimezone(timezone)
+START_TIME = datetime.strptime('2020/04/02 13:00:00', '%Y/%m/%d %H:%M:%S').astimezone(timezone)
+END_TIME = datetime.strptime('2020/04/02 17:00:00', '%Y/%m/%d %H:%M:%S').astimezone(timezone)
 
 # Load Configuration
 try:
@@ -94,6 +94,7 @@ def toggle_stream_targets(queue, start_time=START_TIME, end_time=END_TIME):
         # Get list of stream targets
 
         NOW = datetime.utcnow().replace(tzinfo=pytz.utc)
+        NOW = timezone.localize(NOW)
         try:
             for target in entries:
                 # print(target)
