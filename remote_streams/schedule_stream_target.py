741c7644ab8ba59e5cb05576b05428d5084674f7
Keoni Mahelona <keoni@kmahelona.net>
2020-04-10 15:49:20 +1200

    change settings back for production

--- remote_streams/schedule_stream_target.py
+++ remote_streams/schedule_stream_target.py
@@ -50,13 +50,13 @@ HEADERS ={
     'Content-Type': 'application/json; charset=utf-8'
 }
 
-START_TIME = timezone.localize(datetime.strptime('2020/03/02 11:55:00', '%Y/%m/%d %H:%M:%S'))
+START_TIME = timezone.localize(datetime.strptime('2020/03/02 12:55:00', '%Y/%m/%d %H:%M:%S'))
 END_TIME = timezone.localize(datetime.strptime('2020/03/02 17:00:00', '%Y/%m/%d %H:%M:%S'))
-ENTRIES = ['Face Test', ]#'Push to tehiku.radio', 'Sunshine Radio', ]
+ENTRIES = ['Face Test', 'Push to tehiku.radio', 'Sunshine Radio', ]
 WOWZA_APP_NAME = 'rtmp'
-SOURCE_STREAM_NAME = 'face_test'
+SOURCE_STREAM_NAME = 'teaonews'
 DEFAULT_STREAM_TAKE = 'teaonews'
-DEST_STREAM_NAME = 'teaonews_auto_test'
+DEST_STREAM_NAME = 'teaonews_auto'
 
 # Load Configuration
 try:
