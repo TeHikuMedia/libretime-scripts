54c13986e6658391b4b9814388802f1e7658dc6b
Keoni Mahelona <keoni@kmahelona.net>
2020-05-04 11:48:55 +1200

    change tehiku_fetch to be less often

--- file_cleaning/convert_audio.py
+++ file_cleaning/convert_audio.py
@@ -9,13 +9,15 @@ import json
 from subprocess import Popen, PIPE, call
 
 # m4a doesn't allow a language tag.
-BAD_FORMATS = 'wav mpg wave aiff m4a'
+BAD_FORMATS = 'wav mpg wave aiff'
 
 CONF_FILE = "/etc/librescripts/conf.json"
 # ROOT_FOLDER = "/usr/ubuntu/sync/TeHikuRadioDB"
 ROOT_FOLDER = "/Volumes/Te Hiku Radio Database/"
-ROOT_FOLDER = "/Volumes/Sunshine_Radio_Database/"
+# ROOT_FOLDER = "/Volumes/Sunshine_Radio_Database/"
 LOGFILE= "/var/log/librescripts/update_metadata.log"
+
+ROOT_FOLDER = "/Users/livestream/Resilio Sync/Te Hiku Radio Database/"
 
 logging.basicConfig(
     format='%(asctime)s [%(levelname)s]: %(message)s',
@@ -51,6 +53,10 @@ def scan_folder(ROOT_FOLDER):
                 continue
             elif '~' in root:
                 continue
+            elif 'Trash' in root or 'trash' in root:
+                continue
+            elif '.sync' in root:
+                continue
             elif extension not in 'mp3 mp4 m4a flac wav ogg mpg':
                 logging.debug('Skipping {0}'.format(name))
                 continue
@@ -58,27 +64,39 @@ def scan_folder(ROOT_FOLDER):
             NUM_FILES = NUM_FILES + 1
 
             if extension in BAD_FORMATS:
-                logging.info("Converting {0} to flac".format(FILE_PATH.encode('utf-8')))
+                logging.info("Converting {0} to mp3".format(FILE_PATH.encode('utf-8')))
+                
+
                 out_file = name.split('.')
-                out_file[-1] = 'flac'
+                out_file[-1] = 'mp3'
                 out_file = '.'.join(out_file)
                 cmd = [
                     'ffmpeg', '-y', '-v', 'quiet',
                     '-i', FILE_PATH,
-                    '-c:a', 'flac', os.path.join(root, out_file)
+                    '-c:a', 'libmp3lame', '-q:a', '0', os.path.join(root, out_file)
                 ]
 
+                # logging.info(cmd)
                 p = Popen(cmd, stdout=PIPE, stderr=PIPE)
                 out, err = p.communicate()
                 
                 if os.path.exists(FILE_PATH):
                     logging.info("Converted {0} -> {1}".format(name.encode('utf-8'), out_file.encode('utf-8')))
                     CON_FILES = CON_FILES + 1
+
                     # Move old file to #Trash
-                    trash = os.path.join(ROOT_FOLDER, '~Trash', root)
+                    trash = os.path.join(ROOT_FOLDER, '#TrashConverted')
                     if not os.path.exists(trash):
                         os.mkdir(trash)
-                    call(['mv', FILE_PATH, os.path.join(trash, name)])
+
+                    mv_path = os.path.join(trash, name)
+                    count = 0
+                    while os.path.exists(mv_path):
+                        count = count + 1
+                        mv_path = os.path.join(trash, f'{count}_' + name)
+                    logging.info(f'Moving {FILE_PATH} to {mv_path}')
+                    call(['mv', FILE_PATH, mv_path])
+
                 else:
                     logging.warning("Did not convert {0}".format(name.encode('utf-8')))
 
@@ -90,6 +108,7 @@ def scan_folder(ROOT_FOLDER):
 
 def main():
     for folder in ROOT_FOLDERS:
+        print(ROOT_FOLDERS)
         logging.info('Scanning {0}'.format(folder))
         scan_folder(folder)
 
