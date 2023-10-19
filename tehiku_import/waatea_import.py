4e49433ad8ff30977a49f1f77a365ce92997d83c
Keoni Mahelona <kmahelona@users.noreply.github.com>
2023-10-19 11:53:40 +0200

    update as image is broken

--- tehiku_import/waatea_import.py
+++ tehiku_import/waatea_import.py
@@ -9,7 +9,7 @@ from subprocess import Popen, PIPE
 from math import floor
 from ftplib import FTP
 from os import fchown, path
-from pwd import getpwnam  
+from pwd import getpwnam
 from grp import getgrnam
 from shutil import copyfile
 
@@ -23,21 +23,25 @@ from tehiku_import.add_artwork import add_artwork
 
 timezone = pytz.timezone("Pacific/Auckland")
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--all", help="Download Waatea news items for each hour.", action="store_true")
+parser.add_argument(
+    "-a", "--all", help="Download Waatea news items for each hour.", action="store_true")
 parser.add_argument("-t", "--hour", help="Hour to download")
-parser.add_argument("-x", "--delete", help="Delete all the downloaded files befor continuing.", action="store_true")
+parser.add_argument(
+    "-x", "--delete", help="Delete all the downloaded files befor continuing.", action="store_true")
 args = parser.parse_args()
 
 
 def prepare_folders(path=None):
     if not os.path.exists(BASE_MEDIA_DIR):
         os.mkdir(BASE_MEDIA_DIR)
-        p = Popen(['chown', 'www-data', BASE_MEDIA_DIR], stdin=PIPE, stdout=PIPE)
+        p = Popen(['chown', 'www-data', BASE_MEDIA_DIR],
+                  stdin=PIPE, stdout=PIPE)
         p.communicate()
-        p = Popen(['chgrp', 'www-data', BASE_MEDIA_DIR], stdin=PIPE, stdout=PIPE)
+        p = Popen(['chgrp', 'www-data', BASE_MEDIA_DIR],
+                  stdin=PIPE, stdout=PIPE)
         p.communicate()
 
-    BASE_DIR = os.path.join(BASE_MEDIA_DIR, 'waatea_news')        
+    BASE_DIR = os.path.join(BASE_MEDIA_DIR, 'waatea_news')
     if not os.path.exists(BASE_DIR):
         os.mkdir(BASE_DIR)
         p = Popen(['chown', 'www-data', BASE_DIR], stdin=PIPE, stdout=PIPE)
@@ -67,8 +71,9 @@ def delete_waatea():
 
 
 def get_waatea_all():
-    for hour_increment in range(6,19):
-        time = datetime.strptime('2015-12-09 %02d:00'%(hour_increment),'%Y-%m-%d %H:%M')
+    for hour_increment in range(6, 19):
+        time = datetime.strptime('2015-12-09 %02d:00' %
+                                 (hour_increment), '%Y-%m-%d %H:%M')
         time = timezone.localize(time)
         time = time.astimezone(timezone)
         get_waatea(time)
@@ -82,53 +87,54 @@ def get_waatea_now():
 
 
 def get_waatea_hour(hour):
-    time = datetime.strptime('2015-12-09 %02d:00'%(hour),'%Y-%m-%d %H:%M')
+    time = datetime.strptime('2015-12-09 %02d:00' % (hour), '%Y-%m-%d %H:%M')
     time = timezone.localize(time)
     time = time.astimezone(timezone)
     get_waatea(time)
 
 
 def get_waatea(time):
-    hour = int( time.strftime('%I') ) # always want to get an hour ahead!
+    hour = int(time.strftime('%I'))  # always want to get an hour ahead!
     ampm = time.strftime('%p').split('M')[0].lower()
 
     start_time = datetime.now()
 
-    ftp = FTP('ftp.irirangi.net') 
-    ftp.login('NGWAN_Upload','ngwanupload')
+    ftp = FTP('ftp.irirangi.net')
+    ftp.login('NGWAN_Upload', 'ngwanupload')
     ftp.cwd('MP3_News')
-    items=[]
-    ftp.retrlines('RETR file_ids.txt', lambda x: items.append(x) ).split('\n')
+    items = []
+    ftp.retrlines('RETR file_ids.txt', lambda x: items.append(x)).split('\n')
 
     f_id = ''
     for item in items:
-        if 'news sport %s%s'%(hour,ampm) in item:
+        if 'news sport %s%s' % (hour, ampm) in item:
             f_id = item.split(' ')[0].strip()
     if f_id == '':
         print("No News for this Hour")
         return
 
-    f_name = 'Waatea_News_%s%s.mp3'%(hour,ampm)
+    f_name = 'Waatea_News_%s%s.mp3' % (hour, ampm)
 
     f_path = prepare_folders()
-    tmp_path = os.path.join(BASE_MEDIA_DIR, 'tmp')    
+    tmp_path = os.path.join(BASE_MEDIA_DIR, 'tmp')
     prepare_folders(tmp_path)
-
 
     tmp_file = os.path.join(tmp_path, f_name)
     final_file = os.path.join(f_path, f_name)
 
     target_length = 60*6.0
-    print("Fetching %s"%(f_name))
+    print("Fetching %s" % (f_name))
 
-    xml=[]
-    ftp.retrlines( 'RETR %s.xml'%(f_id), lambda x: xml.append(x) )
+    xml = []
+    ftp.retrlines('RETR %s.xml' % (f_id), lambda x: xml.append(x))
     r_date = ''
     for line in xml:
         if '<recorded>' in line:
-            r_date = line.replace('<recorded>','').replace('</recorded>','').strip()
+            r_date = line.replace('<recorded>', '').replace(
+                '</recorded>', '').strip()
 
-    record_date = timezone.localize(datetime.strptime(r_date,'%m/%d/%Y %H:%M:%S'))
+    record_date = timezone.localize(
+        datetime.strptime(r_date, '%m/%d/%Y %H:%M:%S'))
     print("Recorded", record_date)
 
     get_new_file = False
@@ -150,7 +156,7 @@ def get_waatea(time):
 
     if get_new_file:
         print("Downloading new file...")
-        ftp.retrbinary('RETR %s.MP3'%(f_id), open(tmp_file, 'wb').write)
+        ftp.retrbinary('RETR %s.MP3' % (f_id), open(tmp_file, 'wb').write)
 
         try:
             media_length = scale_media(tmp_file, target_length)
@@ -167,8 +173,9 @@ def get_waatea(time):
         p.communicate()
 
         fd = mutagen.File(final_file, easy=True)
-        fd.tags['DATE'] = record_date.strftime('%Y')        
-        fd.tags['TITLE'] = "%02d%sM "%(hour,ampm.upper()) + 'Waatea News - {0}'.format(record_date.strftime('%a').upper())
+        fd.tags['DATE'] = record_date.strftime('%Y')
+        fd.tags['TITLE'] = "%02d%sM " % (hour, ampm.upper(
+        )) + 'Waatea News - {0}'.format(record_date.strftime('%a').upper())
         fd.tags['ARTIST'] = "Waatea"
         fd.tags['Album'] = "Waatea"
         fd.tags['Language'] = "Māori"
@@ -177,12 +184,15 @@ def get_waatea(time):
         # fd.tags[u'TLEN'] = u"%d:%02d.%d"%(media_length['mins'], media_length['secs'], media_length['hunds'])
         fd.save()
 
-        td =  (datetime.now() - start_time)
-        print('elapsed time = %s' % ( td.seconds ))
+        td = (datetime.now() - start_time)
+        print('elapsed time = %s' % (td.seconds))
 
         # Try to add album art.
-        image_url = 'https://www.waateanews.com/site/uma/images/COLOR-Waatea%20logo%202016-final.jpg'
-        add_artwork(image_url, final_file)
+        image_url = 'https://cdn.tehiku.nz/2022/03/17/704990_waateanews.jpg'
+        try:
+            add_artwork(image_url, final_file)
+        except:
+            pass
 
     else:
         if os.path.exists(tmp_file):
@@ -206,5 +216,4 @@ def main():
 
 if __name__ == "__main__":
 
-    
     main()
