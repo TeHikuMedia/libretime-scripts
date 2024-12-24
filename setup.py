dfaf0c60e5fddd01c7a3dadf2a78b0d92eda1e93
Keoni Mahelona <kmahelona@users.noreply.github.com>
2024-12-24 22:58:18 +1300

    fix install

--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ for file in conf_dict['options']['data_files']:
                      stat.S_IRGRP | stat.S_IROTH)
 
 check_call(
-    ['playwright', 'install', 'install-deps', 'chromium'],
+    ['playwright', 'install-deps', 'chromium'],
     stdout=open(os.devnull, 'wb'), stderr=STDOUT
 )
